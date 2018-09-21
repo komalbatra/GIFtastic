b77238a7fc4460e706e0ca31c59f34f3637a1407
Komal Batra <KomalBatra@gmail.com>
2018-09-21 13:58:45 -0400

    base code working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,65 @@
 // JS file for HOMEWORK #6
 
-$(document).ready(function() {
+// $(document).ready(function() {
+    // Declaring Initial Array for Topics
     var topics = ["Superman", "Aquaman", "Spiderman", "Batman", "Wonder Woman"];
 
     // FUNCTIONS
     //function to display topic buttons
+    function displayInfo(){
+      $("#hero-view").empty();
       var topic = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=jVz8LzEOLKTNPSj1tux4I40dP1PX6Pbg&limit=20";
-     
-            // Creates AJAX call for the specific movie button being clicked
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=jVz8LzEOLKTNPSj1tux4I40dP1PX6Pbg&limit=10";
+
+      // AJAX call to GET information 
       $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+        
+      })
+      .then(function(response) {
+        console.log(queryURL);
+        console.log (response);
 
-        // YOUR CODE GOES HERE!!!
-console.log(response);
+        // Creating a div to hold hero information
+        var results = response.data;
+
+        for (var j = 0; j < results.length; j++){
+        var newTopicDiv = $("<div class='hero-name'>");
+        
+        // Storing the rating data
+        var rating = results[j];
+        console.log (rating);
+                
+        // Creating an element to have the rating displayed
+        var pRating = $("<p>").text("Rating: " + results[j].rating);
+
+        // Displaying the rating
+        newTopicDiv.append(pRating);
+
+        // Retrieving the URL for the GIF
+        var gifURL = results[j].images.fixed_height_still.url;
+
+        // // Creating an element to hold the GIF
+        var gif = $("<img>").attr("src", gifURL);
+
+        // // Appending the image
+        newTopicDiv.append(gif);
+
+        // Putting the entire movie above the previous movies <-----
+        $("#hero-view").prepend(newTopicDiv);
+        }  
       });
-
-    // Function for displaying movie data
+        // .catch(function(error){
+        //   throw error
+        //   console.log(error);
+        // });
+        
+    }
+           
+    // Function for displaying buttons
     function renderButtons() {
-
       // Deletes the movies prior to adding new movies
-      // (this is necessary otherwise you will have repeat buttons)
       $(".buttons-view").empty();
 
       // Loops through the array of movies
@@ -42,7 +79,9 @@ console.log(response);
       }
     }
 
-    // This function handles events where the add movie button is clicked
+
+    //CLICK EVENTS
+    // This function handles events where the add movie button is clicked  <----------
     $("#add-hero").on("click", function(event) {
       event.preventDefault();
       // This line of code will grab the input from the textbox
@@ -57,15 +96,10 @@ console.log(response);
     });
 
     // Adding click event listeners to all elements with a class of "movie"
-    $(document).on("click", ".topic", displayMovieInfo);
+    $(document).on("click", ".topic", displayInfo);
 
     // Calling the renderButtons function to display the intial buttons
     renderButtons();
 
 
-}); //PAGE CLOSING BRACKET
-    // when the page is loaded first.....
-    
-
-    
-        // Declare Variables
+// }); //PAGE CLOSING BRACKET
